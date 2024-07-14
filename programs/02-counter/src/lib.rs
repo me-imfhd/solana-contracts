f582c82076f6c6c8be15a0dae0e62c47be3d7905
Fahad Ahmad <fahadahmad63862@gmail.com>
2024-07-15 04:46:42 +0530

    counter contract complete, (no pda)

---
+++ programs/02-counter/src/lib.rs
@@ -0,0 +1,53 @@
+use anchor_lang::prelude::*;
+
+declare_id!("6a37CHzsrdPt41R4f7v9fmYmmdDVesZ8cFjAPb7aKizY");
+
+#[program]
+pub mod counter {
+    use super::*;
+
+    pub fn create_counter(ctx: Context<CreateCounter>) -> Result<()> {
+        msg!("Creating an counter!");
+        let counter = &mut ctx.accounts.counter;
+        counter.count = 0;
+        counter.user = ctx.accounts.user.key(); // setting signer as user for counter, (when updating we can check if signer and this are same)
+        msg!("Current count is {}", counter.count);
+        msg!("The Admin PubKey is: {} ", counter.user);
+        Ok(())
+    }
+    pub fn increment_counter(ctx: Context<UpdateCounter>) -> Result<()> {
+        msg!("Incrementing the counter!");
+        let counter = &mut ctx.accounts.counter;
+        counter.count += 1;
+        msg!("Current count is {}", counter.count);
+        Ok(())
+    }
+    pub fn decrement_counter(ctx: Context<UpdateCounter>) -> Result<()> {
+        msg!("Decrementing the counter!");
+        let counter = &mut ctx.accounts.counter;
+        counter.count -= 1;
+        msg!("Current count is {}", counter.count);
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct CreateCounter<'info> {
+    #[account(mut)]
+    user: Signer<'info>,
+    #[account(init, payer = user, space = 8 + 32 + 1)]
+    counter: Account<'info, Counter>,
+    system_program: Program<'info, System>,
+}
+#[derive(Accounts)]
+pub struct UpdateCounter<'info> {
+    user: Signer<'info>,
+    #[account(mut, has_one = user)] // this validates that if the user was set properly or not when creating counter
+    counter: Account<'info, Counter>,
+}
+
+#[account]
+pub struct Counter {
+    user: Pubkey,
+    count: u8,
+}
--- tests/counter.ts
+++ tests/counter.ts
@@ -4,13 +4,56 @@ import { Counter } from "../target/types/counter";
 
 describe("counter", () => {
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  let provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
 
   const program = anchor.workspace.Counter as Program<Counter>;
-
-  it("Is initialized!", async () => {
+  const wallet = provider.wallet as anchor.Wallet;
+  const counter_account = new anchor.web3.Keypair();
+  it("Create Counter", async () => {
     // Add your test here.
-    const tx = await program.methods.initialize().rpc();
+    const tx = await program.methods
+      .createCounter()
+      .accounts({ counter: counter_account.publicKey, user: wallet.publicKey })
+      .signers([wallet.payer, counter_account])
+      .rpc();
+
     console.log("Your transaction signature", tx);
+  });
+  it("Fetch a counter!", async () => {
+    const counter = await program.account.counter.fetch(
+      counter_account.publicKey
+    );
+    console.log("Your counter", counter);
+  });
+  it("Increment a counter!", async () => {
+    const tx_1 = await program.methods
+      .incrementCounter()
+      .accounts({ counter: counter_account.publicKey })
+      .rpc();
+    const tx_2 = await program.methods
+      .incrementCounter()
+      .accounts({ counter: counter_account.publicKey })
+      .rpc();
+    const tx_3 = await program.methods
+      .incrementCounter()
+      .accounts({ counter: counter_account.publicKey })
+      .rpc();
+
+    const counterUpdated = await program.account.counter.fetch(
+      counter_account.publicKey
+    );
+    console.log("Your counter count is: ", counterUpdated.count);
+  });
+  it("Decrement a counter!", async () => {
+    const tx_1 = await program.methods
+      .decrementCounter()
+      .accounts({ counter: counter_account.publicKey })
+      .rpc();
+
+    const counterUpdated = await program.account.counter.fetch(
+      counter_account.publicKey
+    );
+    console.log("Your counter count is: ", counterUpdated.count);
   });
 });
